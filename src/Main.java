69f0017092108a045dab14af42ea83752e28d86d
PH86527 <clctrovao2020@gmail.com>
2023-05-18 08:26:40 -0300

    as duas classes sao ligadas uma a outra, sendo uma possivel criar variaveis proprias.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        // Aula de Orientaçao de Objetos
+        //Declarar objetos
+
+        Pessoa adao;
+        //Instanciar obejtos
+
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        //Definiçao do formato
+        adao.nome = "Adao";
+        adao.sobrenome = "Silva";
+
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+        //adicionar comprtamento
+        adao.falar();
+        eva.falar();
+
     }
+
+
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa
+//Atributos da calsse
+{
+    String nome;
+    String sobrenome;
+
+
+    //Método  da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+}
